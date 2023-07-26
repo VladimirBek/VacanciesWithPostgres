bff5c80330d9efebc2efa5948e7e39b7b9d4bcda
VladimirBek <vladimirbek1996@gmail.com>
2023-07-26 23:15:59 +0300

    fixed

--- src/db_creator.py
+++ src/db_creator.py
@@ -1,73 +1,30 @@
-import json
-from configparser import ConfigParser
-
-import psycopg2
-
-
 class DBMaker:
 
-    def __init__(self):
-        self.params = self.config
-
-    @property
-    def employers_ids(self):
-        with open('employers_ids.json', 'r', encoding='utf-8') as file:
-            return json.load(file)
-
-    @property
-    def config(self, filename="database.ini", section="postgresql"):
-        parser = ConfigParser()
-        parser.read(filename)
-        db = {}
-        if parser.has_section(section):
-            params = parser.items(section)
-            for param in params:
-                db[param[0]] = param[1]
-        else:
-            raise Exception(
-                'Section {0} is not found in the {1} file.'.format(section, filename))
-        return db
-
-    def update_config(self, new: dict):
-        return self.params.update(new)
-
-    def create_database(self, db_name):
-        conn = psycopg2.connect(**self.params)
-        conn.autocommit = True
-        with conn.cursor() as cur:
-            cur.execute(f'DROP DATABASE IF EXISTS {db_name}')
-            cur.execute(f'CREATE DATABASE {db_name}')
-        conn.commit()
-        conn.close()
+    @staticmethod
+    def create_database(db_name, cur):
+        cur.execute(f'DROP DATABASE IF EXISTS {db_name}')
+        cur.execute(f'CREATE DATABASE {db_name}')
 
     @staticmethod
     def create_table_vacancies(cur):
-        cur.execute(
-            f'''DROP TABLE IF EXISTS vacancies;
-                CREATE TABLE vacancies(
-                vacancy_id serial PRIMARY KEY,
-                vacancy_name varchar(100),
-                employer_name varchar(50),
-                salary_from integer,
-                salary_to integer,
-                address text,
-                city varchar(50),
-                employer_id smallint
-            )'''
-        )
+        cur.execute('DROP TABLE IF EXISTS vacancies')
+        cur.execute('''CREATE TABLE vacancies(vacancy_id serial PRIMARY KEY,
+                    vacancy_name varchar(100),
+                    employer_name varchar(50),
+                    salary_from integer,
+                    salary_to integer,
+                    address text,
+                    city varchar(50),
+                    employer_id smallint)''')
 
     @staticmethod
     def create_table_employers(cur):
-        cur.execute(
-            f'''DROP TABLE IF EXISTS employers;
-                CREATE TABLE employers(
-                employer_id serial PRIMARY KEY,
-                employer_name varchar(100),
-                address text,
-                city varchar(50),
-                vacancies_count smallint
-                    )'''
-        )
+        cur.execute('DROP TABLE IF EXISTS employers')
+        cur.execute('CREATE TABLE employers(employer_id serial PRIMARY KEY,'
+                    'employer_name varchar(100),'
+                    'address text,'
+                    'city varchar(50),'
+                    'vacancies_count smallint)')
 
     @staticmethod
     def fill_vacancies_table(cur, vacancy_name, employer_name, salary_from, salary_to, address, city, employer_id):
--- src/hh_api.py
+++ src/hh_api.py
@@ -11,14 +11,17 @@ class HHApi:
         try:
             req = requests.get(self.url_vacancies,
                                params={'employer_id': str(employer_id), 'per_page': 100})
-            data = req.json()
-            pages = int(data['pages'])
-            for page in range(pages):
-                req = requests.get(self.url_vacancies,
-                                   params={'employer_id': str(employer_id), 'page': page, 'per_page': 100})
-                result.extend(req.json()['items'])
-                print(f'Страница {page+1} из {pages} загружена')
-            print('Все страницы успешно загружены!')
-            return result
+            if req.status_code == 200:
+                data = req.json()
+                pages = int(data['pages'])
+                for page in range(pages):
+                    req = requests.get(self.url_vacancies,
+                                       params={'employer_id': str(employer_id), 'page': page, 'per_page': 100})
+                    result.extend(req.json()['items'])
+                    print(f'Страница {page+1} из {pages} загружена')
+                print('Все страницы успешно загружены!')
+                return result
+            else:
+                return 'Ошибка подключения'
         except requests.exceptions.RequestException as error:
             print(error)
