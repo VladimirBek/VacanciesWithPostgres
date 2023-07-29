1f86f215262eb674f2b6a17baf644d55fd2dc598
VladimirBek <vladimirbek1996@gmail.com>
2023-07-29 15:09:41 +0300

    DBManager added

---
+++ src/db_manager.py
@@ -0,0 +1,56 @@
+import psycopg2
+
+from src.config import config
+
+
+class DBManager:
+
+    def __init__(self):
+        self.conf = self.params
+
+    @property
+    def params(self):
+        conf = config()
+        conf.update({'dbname': 'ten_employers'})
+        return conf
+
+    def start_connection(self):
+        """Создает подключение к базе данных"""
+        conn = psycopg2.connect(**self.params)
+        return conn
+
+    @staticmethod
+    def get_companies_and_vacancies_count(cur):
+        """Получает список всех компаний и количество вакансий у каждой компании."""
+        cur.execute('SELECT employer_name, vacancies_count FROM employers')
+        rows = cur.fetchall()
+        return rows
+
+    @staticmethod
+    def get_all_vacancies(cur):
+        """Получает список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию."""
+        cur.execute('SELECT vacancy_name, employer_name, salary_from, salary_to, url FROM vacancies')
+        rows = cur.fetchall()
+        return rows
+
+    @staticmethod
+    def get_avg_salary(cur):
+        """Получает среднюю зарплату по вакансиям."""
+        cur.execute('SELECT AVG(salary_from, salary_to) AS avg_salary FROM vacancies')
+        rows = cur.fetchall()
+        return rows
+
+    @staticmethod
+    def get_vacancies_with_higher_salary(cur, avg):
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        cur.execute(f'SELECT * FROM vacancies '
+                    f'WHERE AVG(salary_from, salary_to)>{avg}')
+        rows = cur.fetchall()
+        return rows
+
+    @staticmethod
+    def get_vacancies_with_keyword(cur, key_word):
+        """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”."""
+        cur.execute(f"SELECT * FROM vacancies "
+                    f"WHERE vacancy_name LIKE '%{key_word}%'")
